0d2935a8dcd9a63fa031a52c7dfa7c6166bc74f3
priyanshu101r <62018180+priyanshu101r@users.noreply.github.com>
2026-05-13 12:06:26 +0530

    Add files via upload
    added changes for soldi principle

---
+++ SolidPrinciple/SolidPrinciple/LiskovPrinciple.cs
@@ -0,0 +1,22 @@
+﻿namespace SolidPrinciple
+{
+    public interface IDiscount
+    {
+        decimal CalculateFinalPrice(decimal originalPrice);
+    }
+
+    public class FlatDiscount : IDiscount
+    {
+        public decimal Amount { get; set; }
+        public decimal CalculateFinalPrice(decimal originalPrice)
+            => originalPrice - Amount;
+    }
+    //if later any new type of discount interduced then we can do like this...
+    public class LoyaltyDiscount : IDiscount
+    {
+        public decimal Percentage { get; set; }
+        public decimal CalculateFinalPrice(decimal originalPrice)
+            => originalPrice - (originalPrice * Percentage / 100);
+    }
+    //Now each class only does what it promises, and you avoid misleading behavior.
+}
---
+++ SolidPrinciple/SolidPrinciple/ViolatedLSP.cs
@@ -0,0 +1,23 @@
+﻿namespace SolidPrinciple
+{
+    public class Discount
+    {
+        public virtual decimal Amount { get; set; }
+        public virtual decimal Percentage { get; set; }
+
+        public virtual decimal CalculateFinalPrice(decimal originalPrice)
+        {
+            return originalPrice - Amount - (originalPrice * Percentage / 100);
+        }
+    }
+    public class LoyaltyDiscountt : Discount
+    {
+        public override decimal Amount
+        {
+            get => 0;
+            set { } // Ignore it
+        }
+    }
+
+
+}
